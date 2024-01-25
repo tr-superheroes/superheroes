0ae8e0aa4f6b95232f697d664f5279e405b83bf8
netcam22 <annettelesage@btinternet.com>
2024-01-25 09:01:14 +0000

    feat: add rotate to landscape in start

--- src/components/start-game.tsx
+++ src/components/start-game.tsx
@@ -11,6 +11,16 @@ export const StartGame:React.FC =() =>{
     const [response,setResponse] = useState<TrumpCard[] | undefined>();
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setError] = useState(false);
+    const [orientation, setOrientation] = useState(screen.orientation.type);
+
+    const portraitMode = window.matchMedia("(orientation: portrait)");
+    portraitMode.addEventListener("change", function(event) {
+        if(event.matches) {
+            setOrientation("portrait-primary");
+        } else {
+            setOrientation("landscape-primary");
+        }
+    });
 
     const fetchCards = async() =>{
         try{
@@ -34,32 +44,41 @@ export const StartGame:React.FC =() =>{
     const handleClick = () =>{
         setIsNewGame(false); //hides the button and loads the game container
     }
-
+/*
+    const rotateToLandscape = () => {
+        document.documentElement.requestFullscreen();
+    }
+*/
     return (
-        <GameContext.Provider value={response}>
+        <GameContext.Provider value={response}> 
         {isNewGame &&
         <>
             <div className = "shield">
-            {
             <div className = "shield__text">
                 <p className ="shield__text--main" >Superhero</p>
                 <p className ="shield__text--info">Top</p>
                 <p className ="shield__text--info">Trumps</p>
             </div>
-            }
             </div>
         </>}
         {isError && (<p className="loading-error">Error loading the game</p>)}
         {!isError && isLoading &&  
         <ShowLoading/>
         }
-        {!isError && !isLoading && isNewGame &&
+        {!isError && !isLoading && isNewGame && orientation === "portrait-primary" &&
+            <>
+            <div className = "start">
+                <button className = "button button__game button__game--start">Rotate to Landscape</button>
+            </div>
+            </>
+        }
+        {!isError && !isLoading && isNewGame && orientation === "landscape-primary" &&
         <>
             <div className = "start">
                 <button className = "button button__game button__game--start" onClick={handleClick} >Start Game</button>
             </div>
         </>
-        }   
+        }  
             
             {!isNewGame && 
             <>
